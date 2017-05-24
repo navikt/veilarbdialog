6add4b7c4d676558230722913a61b6f6ffa148c0
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-05-24 20:31:18 +0200

    en mock for veilarbdialog trenger ikke a mocke egne avhengigheter

--- rest/src/test/java/no/nav/fo/veilarbdialog/StartMock.java
+++ rest/src/test/java/no/nav/fo/veilarbdialog/StartMock.java
@@ -1,7 +1,5 @@
 package no.nav.fo.veilarbdialog;
 
-import no.nav.fo.veilarbdialog.util.VarselMock;
-
 import static no.nav.dialogarena.mock.MockServer.startMockServer;
 import static no.nav.fo.veilarbdialog.StartJetty.CONTEXT_NAME;
 import static no.nav.fo.veilarbdialog.StartJetty.PORT;
@@ -9,7 +7,6 @@ import static no.nav.fo.veilarbdialog.StartJetty.PORT;
 public class StartMock {
 
     public static void main(String[] args) throws Exception {
-        VarselMock.init();
         startMockServer(CONTEXT_NAME, PORT);
     }
 
