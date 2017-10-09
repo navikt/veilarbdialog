a2db24a51f6899ad633653af698398b49a13cb2a
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2017-10-09 13:30:30 +0200

    Legg til cache for abac

--- app/src/main/java/no/nav/fo/veilarbdialog/CacheConfig.java
+++ app/src/main/java/no/nav/fo/veilarbdialog/CacheConfig.java
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarbdialog;
 
+import no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOR_ID_FROM_FNR_CACHE;
 import static no.nav.dialogarena.aktor.AktorConfig.FNR_FROM_AKTOR_ID_CACHE;
+import static no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext.ABAC_CACHE;
 
 @Configuration
 @EnableCaching
@@ -18,6 +20,7 @@ public class CacheConfig {
         net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
         config.addCache(FNR_FROM_AKTOR_ID_CACHE);
         config.addCache(AKTOR_ID_FROM_FNR_CACHE);
+        config.addCache(ABAC_CACHE);
         return new EhCacheCacheManager(net.sf.ehcache.CacheManager.newInstance(config));
     }
 }
