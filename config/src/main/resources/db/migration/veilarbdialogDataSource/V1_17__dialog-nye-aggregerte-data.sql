285cab3a82cedc084b468642b4e3c4fc25af63c4
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-01-26 13:38:31 +0100

    [FO-161] ny kolloner for agregerte data

---
+++ config/src/main/resources/db/migration/veilarbdialogDataSource/V1_17__dialog-nye-aggregerte-data.sql
@@ -0,0 +1,5 @@
+ALTER TABLE DIALOG
+  ADD (uleste_meldinger_for_bruker_siden TIMESTAMP,
+  uleste_meldinger_for_veileder_siden TIMESTAMP,
+  venter_pa_nav_siden TIMESTAMP,
+  vener_pa_svar_fra_bruker_siden TIMESTAMP);
