484b60274e1daf98373b80c37f730214cb5c8406
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-02-08 15:14:20 +0100

    [FO-161] byttet navn til feedMateaDataDAO og endret test

---
+++ app/src/test/java/no/nav/fo/veilarbdialog/db/dao/FeedMetaDataDAOTest.java
@@ -0,0 +1,33 @@
+package no.nav.fo.veilarbdialog.db.dao;
+
+import lombok.SneakyThrows;
+import no.nav.fo.IntegrasjonsTest;
+import org.junit.Test;
+
+import javax.inject.Inject;
+import java.util.Date;
+
+import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FeedMetaDataDAOTest extends IntegrasjonsTest {
+
+    @Inject
+    private FeedMetaDataDAO feedMetaDataDAO;
+
+    @Test
+    public void skal_kunne_settte_og_hente_sist_lest_tid() {
+        Date uniktTidspunkt = uniktTidspunkt();
+        feedMetaDataDAO.oppdaterSisteLest(uniktTidspunkt);
+        Date lestTidspunkt = feedMetaDataDAO.hentSisteLestTidspunkt();
+        assertThat(lestTidspunkt).isEqualTo(uniktTidspunkt);
+    }
+
+    @SneakyThrows
+    private Date uniktTidspunkt() {
+        sleep(1);
+        Date date = new Date();
+        sleep(1);
+        return date;
+    }
+}
