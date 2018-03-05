f47127cca9fd6708d15ec04ebd72dda3f4c9690f
Kim Tore Jensen <ambientsound@noreply.users.github.com>
2018-03-05 13:08:50 +0100

    PUS-118: clean up PID numbers from test data.

--- app/src/test/java/no/nav/fo/TestData.java
+++ app/src/test/java/no/nav/fo/TestData.java
@@ -3,8 +3,8 @@ package no.nav.fo;
 public class TestData {
 
     public static final String KJENT_IDENT = "***REMOVED***";
-    public static final String KJENT_AKTOR_ID = "***REMOVED***86";
+    public static final String KJENT_AKTOR_ID = "***REMOVED***42";
 
-    public static final String KJENT_VEILEDER_IDENT = "Z990281";
+    public static final String KJENT_VEILEDER_IDENT = "Z990257";
     public static final String KJENT_IDENT_FOR_KJENT_VEILEDER = "***REMOVED***";
 }
