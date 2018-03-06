a99db078649c3381859197bd5c06f4256341d11d
Torstein Gjengedal <torstein.gjengedal@nav.no>
2018-03-06 15:50:48 +0100

    [FO-4] Tilrettelegger for lokal config som ikke må sjekkes inn i git

--- rest/src/test/java/no/nav/fo/veilarbdialog/StartJetty.java
+++ rest/src/test/java/no/nav/fo/veilarbdialog/StartJetty.java
@@ -9,6 +9,7 @@ import no.nav.fo.DatabaseTestContext;
 import no.nav.fo.veilarbdialog.util.VarselMock;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 
+import static java.lang.System.setProperty;
 import static no.nav.dialogarena.config.DevelopmentSecurity.setupISSO;
 import static no.nav.fo.veilarbdialog.ApplicationContext.APPLICATION_NAME;
 import static no.nav.fo.veilarbdialog.db.DatabaseContext.AKTIVITET_DATA_SOURCE_JDNI_NAME;
@@ -16,6 +17,11 @@ import static no.nav.fo.veilarbdialog.util.StringUtils.of;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
 import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
 import static no.nav.testconfig.ApiAppTest.setupTestContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -25,6 +31,7 @@ public class StartJetty {
     private static final int SSL_PORT = 8585;
 
     public static void main(String[] args) throws Exception {
+        loadPropertiesFile("/environment-local.properties");
         Jetty jetty = setupISSO(usingWar()
                         .at(APPLICATION_NAME)
                         .loadProperties("/environment-test.properties")
@@ -37,6 +44,20 @@ public class StartJetty {
         setupTestContext();
 
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
+    }
+
+    private static void loadPropertiesFile(String propertyFile) {
+        Properties properties = new Properties();
+        try {
+            properties.load(System.class.getResourceAsStream(propertyFile));
+        } catch (IOException e) {
+            //Ignores non-exisiting file
+            return;
+        }
+
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            setProperty((String) entry.getKey(), (String) entry.getValue());
+        }
     }
 
     private static DataSource createDataSource() {
