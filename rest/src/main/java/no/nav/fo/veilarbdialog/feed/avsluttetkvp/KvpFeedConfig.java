ccc03b313864f6af078f784ca95e8d1063be553e
Torstein Gjengedal <torstein.gjengedal@nav.no>
2018-05-23 11:32:05 +0200

    [FO-1074] Bruker shedlock ved innlesing av feed for avsluttet kvp

--- rest/src/main/java/no/nav/fo/veilarbdialog/feed/avsluttetkvp/KvpFeedConfig.java
+++ rest/src/main/java/no/nav/fo/veilarbdialog/feed/avsluttetkvp/KvpFeedConfig.java
@@ -1,15 +1,20 @@
 package no.nav.fo.veilarbdialog.feed.avsluttetkvp;
 
-import no.nav.brukerdialog.security.oidc.OidcFeedOutInterceptor;
-import no.nav.fo.feed.consumer.FeedConsumer;
-import no.nav.fo.feed.consumer.FeedConsumerConfig;
-import no.nav.fo.veilarboppfolging.rest.domain.KvpDTO;
+import java.util.Collections;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbc.JdbcLockProvider;
+import no.nav.brukerdialog.security.oidc.OidcFeedOutInterceptor;
+import no.nav.fo.feed.consumer.FeedConsumer;
+import no.nav.fo.feed.consumer.FeedConsumerConfig;
+import no.nav.fo.veilarboppfolging.rest.domain.KvpDTO;
 
 @Configuration
 public class KvpFeedConfig {
@@ -19,6 +24,14 @@ public class KvpFeedConfig {
 
     @Value("${kvp.feed.consumer.pollingrate:/10 * * * * ?}")
     private String polling;
+
+    @Inject
+    private DataSource dataSource;
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcLockProvider(dataSource);
+    }    
 
     @Bean
     public FeedConsumer<KvpDTO> kvpFeedDTOFeedConsumer(KvpFeedConsumer kvpFeedConsumer) {
@@ -31,6 +44,7 @@ public class KvpFeedConfig {
                 ),
                 new FeedConsumerConfig.CronPollingConfig(polling)
         )
+                .lockProvider(lockProvider(dataSource), 10000)
                 .callback(kvpFeedConsumer::lesKvpFeed)
                 .interceptors(Collections.singletonList(new OidcFeedOutInterceptor()));
 
