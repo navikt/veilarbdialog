4c8754c848213b3c61e5722116d328233272e1dc
Torstein Gjengedal <torstein.gjengedal@nav.no>
2018-08-23 11:06:22 +0200

    [KAIZEN-0] Fikser kompileringsfeil

--- ws/src/test/java/no/nav/fo/StartJetty.java
+++ ws/src/test/java/no/nav/fo/StartJetty.java
@@ -4,7 +4,6 @@ import no.nav.dialogarena.config.DevelopmentSecurity;
 import no.nav.fo.veilarbdialog.util.VarselMock;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 
-import static no.nav.fo.IntegrasjonsTest.APPLICATION_NAME;
 import static no.nav.fo.veilarbdialog.db.DatabaseContext.AKTIVITET_DATA_SOURCE_JDNI_NAME;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
 import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
@@ -12,6 +11,8 @@ import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
 public class StartJetty {
     private static final int PORT = 8581;
 
+    public static final String APPLICATION_NAME = "veilarbdialog";
+    
     public static void main(String[] args) throws Exception {
         Jetty jetty = DevelopmentSecurity.setupSamlLogin(usingWar()
                         .at("/veilarbdialog-ws")
