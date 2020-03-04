3cabef79cfd8c141497dbb48781077e8ca47f9d9
alexandraback <alexandra1.back@gmail.com>
2020-03-04 09:17:04 +0100

    FO-2704: Annotate:a kafkadialogservice

--- src/main/java/no/nav/fo/veilarbdialog/service/KafkaDialogService.java
+++ src/main/java/no/nav/fo/veilarbdialog/service/KafkaDialogService.java
@@ -8,11 +8,15 @@ import no.nav.fo.veilarbdialog.domain.KafkaDialogMelding;
 import no.nav.json.JsonUtils;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
 import java.util.List;
 import java.util.stream.Collectors;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 @Slf4j
+@Component
 public class KafkaDialogService  {
 
     private KafkaDAO kafkaDAO;
@@ -22,13 +26,14 @@ public class KafkaDialogService  {
     private static final String APP_ENVIRONMENT_NAME = "APP_ENVIRONMENT_NAME";
     static final String KAFKA_PRODUCER_TOPIC = "aapen-fo-endringPaaDialog-v1" + "-" + getRequiredProperty(APP_ENVIRONMENT_NAME);
 
+    @Inject
     public KafkaDialogService(Producer<String, String> kafkaProducer, KafkaDAO kafkaDAO, DialogDAO dialogDAO) {
         this.kafkaDAO = kafkaDAO;
         this.kafkaProducer = kafkaProducer;
         this.dialogDAO = dialogDAO;
     }
 
-    public void dialogEvent (KafkaDialogMelding kafkaDialogMelding) {
+    public void dialogEvent(KafkaDialogMelding kafkaDialogMelding) {
         String kafkaStringMelding = JsonUtils.toJson(kafkaDialogMelding);
         ProducerRecord<String, String> kafkaMelding = new ProducerRecord<>(KAFKA_PRODUCER_TOPIC, kafkaStringMelding);
         kafkaProducer.send(kafkaMelding, (metadata, exception) -> {
@@ -45,13 +50,14 @@ public class KafkaDialogService  {
         });
     }
 
-    public void sendAlleFeilendeMeldinger () {
+    public void sendAlleFeilendeMeldinger() {
         kafkaDAO.hentAlleFeilendeAktorId()
                 .stream()
                 .map(aktorId -> {
                     List<DialogData> dialoger =  dialogDAO.hentDialogerForAktorId(aktorId);
                     return KafkaDialogMelding.mapTilDialogData(dialoger, aktorId);
                 })
-                .collect(Collectors.toList()).forEach(this::dialogEvent);
+                .collect(Collectors.toList())
+                .forEach(this::dialogEvent);
     }
 }
