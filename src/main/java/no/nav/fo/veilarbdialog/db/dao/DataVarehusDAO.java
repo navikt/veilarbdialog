c215032f41239f019553475e09aefe4b0a059ff7
Cato Olsen <cato.olsen@nav.no>
2020-08-21 17:20:17 +0200

    FO-2962: Using JDBCTemplate.

--- src/main/java/no/nav/fo/veilarbdialog/db/dao/DataVarehusDAO.java
+++ src/main/java/no/nav/fo/veilarbdialog/db/dao/DataVarehusDAO.java
@@ -1,22 +1,18 @@
 package no.nav.fo.veilarbdialog.db.dao;
 
-import no.nav.common.auth.SubjectHandler;
+import lombok.RequiredArgsConstructor;
+import no.nav.common.auth.subject.SubjectHandler;
 import no.nav.fo.veilarbdialog.domain.DatavarehusEvent;
 import no.nav.fo.veilarbdialog.domain.DialogData;
-import no.nav.sbl.jdbc.Database;
-import no.nav.sbl.sql.SqlUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.inject.Inject;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Date;
-
-import static no.nav.sbl.sql.DbConstants.CURRENT_TIMESTAMP;
+import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class DataVarehusDAO {
 
     static final String EVENT_ID = "event_id";
@@ -25,53 +21,34 @@ public class DataVarehusDAO {
     static final String TIDSPUNKT = "tidspunkt";
     static final String AKTOR_ID = "aktor_id";
     static final String AKTIVITET_ID = "aktivitet_id";
-    static final String LAGT_INN_AV = "lagt_inn_av";
     static final String EVENT_TABELL = "EVENT";
 
     private final JdbcTemplate jdbc;
-    private final Database database;
-
-    @Inject
-    public DataVarehusDAO(JdbcTemplate jdbcTemplate, Database database) {
-        this.jdbc = jdbcTemplate;
-        this.database = database;
-    }
 
     public void insertEvent(DialogData dialogData, DatavarehusEvent datavarehusEvent) {
-        Long nextSeq = SqlUtils.select(jdbc, "dual", DataVarehusDAO::hentLongFraForsteKollone)
-                .column("EVENT_ID_SEQ.NEXTVAL")
-                .execute();
-
-        SqlUtils.insert(jdbc, EVENT_TABELL)
-                .value(EVENT_ID, nextSeq)
-                .value(DIALOGID, dialogData.getId())
-                .value(EVENT, datavarehusEvent.toString())
-                .value(TIDSPUNKT, CURRENT_TIMESTAMP)
-                .value(AKTOR_ID, dialogData.getAktorId())
-                .value(AKTIVITET_ID, dialogData.getAktivitetId())
-                .value(LAGT_INN_AV, getLagtInnAv())
-                .execute();
+        long nextId = Optional
+                .ofNullable(jdbc.queryForObject("select EVENT_ID_SEQ.NEXTVAL from dual", Long.class))
+                .orElseThrow(IllegalStateException::new);
+        jdbc.update("insert into EVENT (event_id, dialogid, event, tidspunkt, aktor_id, aktivitet_id, lagt_inn_av) values (?, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?)",
+                nextId,
+                dialogData.getId(),
+                datavarehusEvent.toString(),
+                dialogData.getAktorId(),
+                dialogData.getAktivitetId(),
+                getLagtInnAv());
     }
 
     @Transactional(readOnly = true)
     public Date hentSisteEndringSomIkkeErDine(String aktorId, String bruker) {
-        return database.query(
-                "SELECT TIDSPUNKT from EVENT where AKTOR_ID = ? and LAGT_INN_AV != ? ORDER BY EVENT_ID DESC FETCH FIRST 1 ROWS ONLY",
-                rs -> Database.hentDato(rs, "TIDSPUNKT"),
-                aktorId,
-                bruker
-        )
-                .stream()
-                .findFirst()
-                .orElse(null);
+        return jdbc.queryForObject("select TIDSPUNKT from EVENT where AKTOR_ID = ? and LAGT_INN_AV != ? ORDER BY EVENT_ID DESC FETCH FIRST 1 ROWS ONLY",
+                new Object[]{
+                        aktorId,
+                        bruker
+                },
+                Date.class);
     }
 
-
-    private static Long hentLongFraForsteKollone(ResultSet resultSet) throws SQLException {
-        return resultSet.getLong(1);
-    }
-
-    private String getLagtInnAv() {
+    private static String getLagtInnAv() {
         return SubjectHandler.getIdent().orElse("SYSTEM");
     }
 
