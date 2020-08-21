f3345eab1e2e525f230624d1c3110fe49451a4a3
Cato Olsen <cato.olsen@nav.no>
2020-08-21 14:31:46 +0200

    FO-2962: Using JDBCTemplate.

--- src/main/java/no/nav/fo/veilarbdialog/db/dao/FeedMetaDataDAO.java
+++ src/main/java/no/nav/fo/veilarbdialog/db/dao/FeedMetaDataDAO.java
@@ -1,33 +1,25 @@
 package no.nav.fo.veilarbdialog.db.dao;
 
-import no.nav.sbl.jdbc.Database;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.inject.Inject;
 import java.util.Date;
 
 @Component
+@RequiredArgsConstructor
 public class FeedMetaDataDAO {
-    private final Database database;
 
-    @Inject
-    public FeedMetaDataDAO(Database database) {
-        this.database = database;
-    }
+    private final JdbcTemplate jdbc;
 
     public Date hentSisteLestTidspunkt() {
-        return database.queryForObject(
-                "SELECT tidspunkt_siste_endring " +
-                        "FROM FEED_METADATA",
-                (rs) -> Database.hentDato(rs, "tidspunkt_siste_endring")
-        );
+        return jdbc.queryForObject("select tidspunkt_siste_endring from FEED_METADATA",
+                Date.class);
     }
 
     public void oppdaterSisteLest(Date date) {
-        database.update(
-                "UPDATE FEED_METADATA SET tidspunkt_siste_endring = ?",
-                date
-        );
+        jdbc.update("update FEED_METADATA set tidspunkt_siste_endring = ?",
+                date);
     }
 
 }
