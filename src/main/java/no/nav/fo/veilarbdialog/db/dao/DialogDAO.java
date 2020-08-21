4809aa534cd6657cf53828625efb5f3b9eeec457
theagilepadawan <michael.gundersen@nav.no>
2020-08-21 15:15:21 +0200

    FO-2695: Endret teksten for kaserte enkelthenvendelser.

--- src/main/java/no/nav/fo/veilarbdialog/db/dao/DialogDAO.java
+++ src/main/java/no/nav/fo/veilarbdialog/db/dao/DialogDAO.java
@@ -91,7 +91,7 @@ public class DialogDAO {
     }
 
     public int kasserHenvendelse(long id) {
-        return database.update("UPDATE HENVENDELSE SET TEKST = 'Kassert av NAV' WHERE HENVENDELSE_ID = ?", id);
+        return database.update("UPDATE HENVENDELSE SET TEKST = 'Det var skrevet noe feil, og det er nå slettet.' WHERE HENVENDELSE_ID = ?", id);
     }
 
     public int kasserDialog(long id) {
