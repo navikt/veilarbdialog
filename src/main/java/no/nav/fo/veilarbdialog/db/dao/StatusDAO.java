4e208912d5718a2d6c10d8a02fbb53b6b09823de
Cato Olsen <cato.olsen@nav.no>
2020-08-21 13:28:36 +0200

    FO-2962: Using JDBCTemplate.

--- src/main/java/no/nav/fo/veilarbdialog/db/dao/StatusDAO.java
+++ src/main/java/no/nav/fo/veilarbdialog/db/dao/StatusDAO.java
@@ -1,100 +1,131 @@
 package no.nav.fo.veilarbdialog.db.dao;
 
-import no.nav.sbl.jdbc.Database;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.inject.Inject;
 import java.util.Date;
-
-import static no.nav.fo.veilarbdialog.db.dao.DBKonstanter.*;
 
 @Component
 @Transactional
+@RequiredArgsConstructor
 public class StatusDAO {
 
-    private final Database db;
+    private final JdbcTemplate jdbc;
     private final DateProvider dateProvider;
 
-    @Inject
-    public StatusDAO(Database db, DateProvider dateProvider) {
-        this.db = db;
-        this.dateProvider = dateProvider;
-    }
-
     public void markerSomLestAvVeileder(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                ELDSTE_ULESTE_FOR_VEILEDER + " = null, " +
-                LEST_AV_VEILEDER_TID + " = " + dateProvider.getNow() + ", " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "eldste_uleste_for_veileder = ?, " +
+                        "lest_av_veileder_tid = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                null,
+                dateProvider.getNow(),
+                dateProvider.getNow(),
+                dialogId
+        );
     }
 
     public void markerSomLestAvBruker(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                HAR_ULEST_PARAGRAF_8 + " = 0, " +
-                ELDSTE_ULESTE_FOR_BRUKER + " = null, " +
-                LEST_AV_BRUKER_TID + " = " + dateProvider.getNow() + ", " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "ulestParagraf8Varsel = ?, " +
+                        "eldste_uleste_for_bruker = ?, " +
+                        "lest_av_bruker_tid = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                0,
+                null,
+                dateProvider.getNow(),
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void setVenterPaNavTilNaa(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                VENTER_PA_NAV_SIDEN + " = " + dateProvider.getNow() + ", " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "venter_pa_nav_siden = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                dateProvider.getNow(),
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void setVenterPaSvarFraBrukerTilNaa(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                VENTER_PA_SVAR_FRA_BRUKER + " = " + dateProvider.getNow() + ", " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "venter_pa_svar_fra_bruker = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                dateProvider.getNow(),
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void setVenterPaNavTilNull(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                VENTER_PA_NAV_SIDEN + " = null, " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "venter_pa_nav_siden = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                null,
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void setVenterPaSvarFraBrukerTilNull(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                VENTER_PA_SVAR_FRA_BRUKER + " = null, " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "venter_pa_svar_fra_bruker = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                null,
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void setEldsteUlesteForBruker(long dialogId, Date date) {
-        db.update("UPDATE DIALOG SET " +
-                ELDSTE_ULESTE_FOR_BRUKER + " = ?, " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", date, dialogId);
+        jdbc.update("update DIALOG set " +
+                        "eldste_uleste_for_bruker = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                date,
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void setNyMeldingFraBruker(long dialogId, Date eldsteUlesteForVeileder, Date venterPaNavSiden) {
-        db.update("UPDATE DIALOG SET " +
-                VENTER_PA_SVAR_FRA_BRUKER + " = null, " +
-                ELDSTE_ULESTE_FOR_VEILEDER + " = ?, " +
-                VENTER_PA_NAV_SIDEN + " = ?, " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", eldsteUlesteForVeileder, venterPaNavSiden, dialogId);
+        jdbc.update("update DIALOG set " +
+                        "venter_pa_svar_fra_bruker = ?, " +
+                        "eldste_uleste_for_veileder = ?, " +
+                        "venter_pa_nav_siden = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                null,
+                eldsteUlesteForVeileder,
+                venterPaNavSiden,
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void setHistorisk(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                VENTER_PA_SVAR_FRA_BRUKER + " = null, " +
-                VENTER_PA_NAV_SIDEN + " = null, " +
-                HISTORISK + " = 1, " +
-                OPPDATERT + " = " + dateProvider.getNow() + " " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "venter_pa_svar_fra_bruker = ?, " +
+                        "venter_pa_nav_siden = ?, " +
+                        "historisk = ?, " +
+                        "oppdatert = ? " +
+                        "where dialog_id = ?",
+                null,
+                null,
+                1,
+                dateProvider.getNow(),
+                dialogId);
     }
 
     public void markerSomParagraf8(long dialogId) {
-        db.update("UPDATE DIALOG SET " +
-                HAR_ULEST_PARAGRAF_8 + " = 1 " +
-                "WHERE " + DIALOG_ID + " = ?", dialogId);
+        jdbc.update("update DIALOG set " +
+                        "ulestParagraf8Varsel = ? " +
+                        "where dialog_id = ?",
+                1,
+                dialogId);
     }
+
 }
