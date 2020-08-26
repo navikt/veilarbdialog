0e8ce08ff84f6c2e00808e8a68ce5252383d2fb2
Cato Olsen <cato.olsen@nav.no>
2020-08-26 16:44:19 +0200

    FO-2962: Kopiert fra gammelt oppsett i AktorConfig/AbacContext.

--- src/main/java/no/nav/fo/veilarbdialog/config/CacheConfig.java
+++ src/main/java/no/nav/fo/veilarbdialog/config/CacheConfig.java
@@ -1,18 +1,36 @@
 package no.nav.fo.veilarbdialog.config;
 
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.PersistenceConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static no.nav.dialogarena.aktor.AktorConfig.AKTOR_ID_FROM_FNR_CACHE;
-import static no.nav.dialogarena.aktor.AktorConfig.FNR_FROM_AKTOR_ID_CACHE;
-import static no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext.ABAC_CACHE;
+import static net.sf.ehcache.config.PersistenceConfiguration.Strategy.LOCALTEMPSWAP;
+import static net.sf.ehcache.store.MemoryStoreEvictionPolicy.LRU;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+    private static final CacheConfiguration FNR_FROM_AKTOR_ID_CACHE = new CacheConfiguration("fnrFromAktorId", 100000)
+            .memoryStoreEvictionPolicy(LRU)
+            .timeToIdleSeconds(86400)
+            .timeToLiveSeconds(86400)
+            .persistence(new PersistenceConfiguration().strategy(LOCALTEMPSWAP));
+
+    private static final CacheConfiguration AKTOR_ID_FROM_FNR_CACHE = new CacheConfiguration("aktorIdFromFnr", 100000)
+            .memoryStoreEvictionPolicy(LRU)
+            .timeToIdleSeconds(86400)
+            .timeToLiveSeconds(86400)
+            .persistence(new PersistenceConfiguration().strategy(LOCALTEMPSWAP));
+
+    private static final CacheConfiguration ABAC_CACHE = new CacheConfiguration("askForPermission", 10000)
+            .memoryStoreEvictionPolicy(LRU)
+            .timeToIdleSeconds(3600)
+            .timeToLiveSeconds(3600);
 
     @Bean
     public CacheManager cacheManager() {
@@ -22,4 +40,5 @@ public class CacheConfig {
         config.addCache(ABAC_CACHE);
         return new EhCacheCacheManager(net.sf.ehcache.CacheManager.newInstance(config));
     }
+
 }
