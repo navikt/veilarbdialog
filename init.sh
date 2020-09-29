ab90b938f7bf17011787c89ef8eb1c0540f03415
Cato Olsen <cato.olsen@nav.no>
2020-09-29 21:34:53 +0200

    Oracle credentials exported to environment, Spring Boot style.

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 export SERVICEUSER_USERNAME=$(cat /secrets/serviceuser/username)
 export SERVICEUSER_PASSWORD=$(cat /secrets/serviceuser/password)
+export VEILARBDIALOGDATASOURCE_USERNAME=$(cat /var/run/secrets/nais.io/oracle_creds/username)
+export VEILARBDIALOGDATASOURCE_PASSWORD=$(cat /var/run/secrets/nais.io/oracle_creds/password)
