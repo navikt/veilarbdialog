86b2a7bb22728d0cfe2b73f32416fcbd317f37d6
hakon planke holm <hakon.planke.holm@nav.no>
2020-10-30 13:35:12 +0100

    fiks dao

--- src/main/java/no/nav/fo/veilarbdialog/db/dao/DialogDAO.java
+++ src/main/java/no/nav/fo/veilarbdialog/db/dao/DialogDAO.java
@@ -123,15 +123,14 @@ public class DialogDAO {
         log.info("Date provider is {} and value is {}", dateProvider, dateProvider.getNow());
 
         jdbc.update("insert into DIALOG (DIALOG_ID, AKTOR_ID, OPPRETTET_DATO, AKTIVITET_ID, OVERSKRIFT, HISTORISK, KONTORSPERRE_ENHET_ID, OPPDATERT) " +
-                        "values (?, ?, ?, ?, ?, ?, ?, ?)",
+                        "values (?, ?, ?, ?, ?, ?, ?, " +dateProvider.getNow() +")",
                 dialogId,
                 dialogData.getAktorId(),
                 dateProvider.getNow(),
                 dialogData.getAktivitetId(),
                 dialogData.getOverskrift(),
                 dialogData.isHistorisk() ? 1 : 0,
-                dialogData.getKontorsperreEnhetId(),
-                dateProvider.getNow());
+                dialogData.getKontorsperreEnhetId());
 
         dialogData.getEgenskaper()
                 .forEach(egenskapType -> updateDialogEgenskap(egenskapType, dialogId));
@@ -153,10 +152,9 @@ public class DialogDAO {
                 .orElseThrow(IllegalStateException::new);
 
         jdbc.update("insert into HENVENDELSE (HENVENDELSE_ID, DIALOG_ID, SENDT, TEKST, KONTORSPERRE_ENHET_ID, AVSENDER_ID, AVSENDER_TYPE, VIKTIG) " +
-                        "values (?, ?, ?, ?, ?, ?, ?, ?)",
+                        "values (?, ?, ?, ?, ?, ?, " + dateProvider.getNow() + ", ?)",
                 henvendelseId,
                 henvendelseData.dialogId,
-                dateProvider.getNow(),
                 henvendelseData.tekst,
                 henvendelseData.kontorsperreEnhetId,
                 henvendelseData.avsenderId,
