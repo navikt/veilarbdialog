5a71ac75de908781efae5caaa5d6ece882df2778
hakon planke holm <hakon.planke.holm@nav.no>
2020-12-22 10:33:52 +0100

    teste kaseringsfiks

--- src/main/java/no/nav/fo/veilarbdialog/kassering/CustomWebMvcConfigurer.java
+++ src/main/java/no/nav/fo/veilarbdialog/kassering/CustomWebMvcConfigurer.java
@@ -1,11 +1,17 @@
 package no.nav.fo.veilarbdialog.kassering;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CustomWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/public/");
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
