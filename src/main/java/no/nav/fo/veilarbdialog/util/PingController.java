9d1695c297fad92c77e8633b92677ebee8cac669
hakon planke holm <hakon.planke.holm@nav.no>
2021-04-08 16:48:36 +0200

    fjerne ubrukt import

--- src/main/java/no/nav/fo/veilarbdialog/util/PingController.java
+++ src/main/java/no/nav/fo/veilarbdialog/util/PingController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
