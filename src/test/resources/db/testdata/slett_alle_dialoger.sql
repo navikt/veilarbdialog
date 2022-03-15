875b63d5c027de88532bf042a92656680d1dda08
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-03-15 13:59:15 +0100

    Fikse tester

--- src/test/resources/db/testdata/slett_alle_dialoger.sql
+++ src/test/resources/db/testdata/slett_alle_dialoger.sql
@@ -6,3 +6,4 @@ delete from DIALOG_AKTOR where (select 1 from dual) = 1;
 delete from DIALOG_EGENSKAP where (select 1 from dual) = 1;
 delete from DIALOG where (select 1 from dual) = 1;
 delete from EVENT;
+delete from VARSEL;
