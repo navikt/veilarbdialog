836393f4904fa65df6cc99b899d9029f2ab00eae
hakonph <hakon.planke.holm@nav.no>
2022-04-20 15:09:04 +0200

    fiks schedlock (#193)

--- src/main/java/no/nav/fo/veilarbdialog/Application.java
+++ src/main/java/no/nav/fo/veilarbdialog/Application.java
@@ -1,8 +1,10 @@
 package no.nav.fo.veilarbdialog;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
 public class Application {
 
     public static void main(String... args) {
