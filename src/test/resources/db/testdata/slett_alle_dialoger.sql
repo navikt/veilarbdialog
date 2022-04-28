1bf4322787705fa61dd8a204153a638eb5bd946d
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-04-28 14:35:01 +0200

    Rydde testskript

--- src/test/resources/db/testdata/slett_alle_dialoger.sql
+++ src/test/resources/db/testdata/slett_alle_dialoger.sql
@@ -1,5 +1,4 @@
 delete from HENVENDELSE where (select 1 from dual) = 1;
-delete from ESKALERINGSVARSEL_GJELDENDE where (select 1 from dual) = 1;
 delete from ESKALERINGSVARSEL where (select 1 from dual) = 1;
 delete from BRUKERNOTIFIKASJON where (select 1 from dual) = 1;
 
