80de8bf2da4b2caeb01ed84e8b44f25f8945dc93
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-07-29 16:25:55 +0200

    Bytte ut aktørregisterklient med pdlklient (#209)
    * Litt housekeeping
    
    * Skifter ut aktørregisterklient med pdl-klient + litt housekeeping
    
    * Debug logging - temporær
    
    * Fjernet debug logging
    
    * Endret namespace for pdl-api

--- src/main/java/no/nav/fo/veilarbdialog/config/AktorClientConfig.java
+++ src/main/java/no/nav/fo/veilarbdialog/config/AktorClientConfig.java
@@ -1,27 +1,32 @@
 package no.nav.fo.veilarbdialog.config;
 
 import no.nav.common.client.aktoroppslag.AktorOppslagClient;
-import no.nav.common.client.aktoroppslag.AktorregisterHttpClient;
 import no.nav.common.client.aktoroppslag.CachedAktorOppslagClient;
-import no.nav.common.client.aktorregister.AktorregisterClient;
+import no.nav.common.client.aktoroppslag.PdlAktorOppslagClient;
 import no.nav.common.sts.SystemUserTokenProvider;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import static no.nav.common.utils.UrlUtils.createServiceUrl;
 
 @Configuration
 public class AktorClientConfig {
-    @Value("${application.aktorRegister.url}")
-    private String aktorRegisterUrl;
-    @Value("${spring.application.name}")
-    private String applicationName;
 
     @Bean
-    public AktorOppslagClient aktorOppslagClient(SystemUserTokenProvider tokenProvider) {
-        AktorregisterClient aktorregisterClient = new AktorregisterHttpClient(
-            aktorRegisterUrl, applicationName, tokenProvider::getSystemUserToken
+    @Profile("!local")
+    public String pdlUrl() {
+        return createServiceUrl("pdl-api", "pdl", false);
+    }
+
+    @Bean
+    public AktorOppslagClient aktorOppslagClient(String pdlUrl, SystemUserTokenProvider systemUserTokenProvider) {
+        AktorOppslagClient aktorOppslagClient = new PdlAktorOppslagClient(
+                pdlUrl,
+                systemUserTokenProvider::getSystemUserToken,
+                systemUserTokenProvider::getSystemUserToken
         );
 
-        return new CachedAktorOppslagClient(aktorregisterClient);
+        return new CachedAktorOppslagClient(aktorOppslagClient);
     }
 }
