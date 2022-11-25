0d679cdd23886eb7ed317860cf6f63ce9481b160
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-11-25 09:11:06 +0100

    Endrer tekster fra Ditt NAV til Min side

--- src/main/java/no/nav/fo/veilarbdialog/brukernotifikasjon/BrukernotifikasjonTekst.java
+++ src/main/java/no/nav/fo/veilarbdialog/brukernotifikasjon/BrukernotifikasjonTekst.java
@@ -4,12 +4,12 @@ public class BrukernotifikasjonTekst {
 
     private BrukernotifikasjonTekst() {
     }
-    public static final String BESKJED_SMS_TEKST = "Hei! Du har fått en ny beskjed på Ditt NAV. Logg inn og se hva beskjeden gjelder. Vennlig hilsen NAV";
-    public static final String BESKJED_EPOST_TITTEL = "Du har fått en ny beskjed på Ditt NAV";
-    public static final String BESKJED_EPOST_BODY = "Hei! Du har fått en ny beskjed på Ditt NAV. Logg inn og se hva beskjeden gjelder. Vennlig hilsen NAV";
+    public static final String BESKJED_SMS_TEKST = "Hei! Du har fått en ny beskjed på Min side. Logg inn og se hva beskjeden gjelder. Vennlig hilsen NAV";
+    public static final String BESKJED_EPOST_TITTEL = "Du har fått en ny beskjed på Min side";
+    public static final String BESKJED_EPOST_BODY = "Hei! Du har fått en ny beskjed på Min side. Logg inn og se hva beskjeden gjelder. Vennlig hilsen NAV";
     public static final String BESKJED_BRUKERNOTIFIKASJON_TEKST = "Ny melding i dialogen med veilederen din";
 
-    public static final String OPPGAVE_SMS_TEKST = "Hei! Du har fått en ny viktig oppgave på Ditt NAV. Logg inn og se hva oppgaven gjelder. Vennlig hilsen NAV";
+    public static final String OPPGAVE_SMS_TEKST = "Hei! Du har fått en ny viktig oppgave på Min side. Logg inn og se hva oppgaven gjelder. Vennlig hilsen NAV";
     public static final String OPPGAVE_EPOST_TITTEL = "Viktig oppgave";
     public static final String OPPGAVE_EPOST_BODY = "Du har fått en ny viktig oppgave fra NAV. Logg inn og se hva oppgaven gjelder. Vennlig hilsen NAV";
     public static final String OPPGAVE_BRUKERNOTIFIKASJON_TEKST = "Viktig oppgave. NAV vurderer å stanse pengene dine. Se hva du må gjøre.";
