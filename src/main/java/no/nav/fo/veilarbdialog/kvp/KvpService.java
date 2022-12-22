0aae9cfdfdcd3ac0e5989313019db0ce34eaea9c
sigurdgroneng <sigurd.groneng@nav.no>
2022-12-22 11:10:48 +0100

    Extract logic to portefolje-client

--- src/main/java/no/nav/fo/veilarbdialog/kvp/KvpService.java
+++ src/main/java/no/nav/fo/veilarbdialog/kvp/KvpService.java
@@ -20,7 +20,6 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
-@Service
 @RequiredArgsConstructor
 public class KvpService {
 
--- src/main/java/no/nav/fo/veilarbdialog/oppfolging/v2/OppfolgingV2ClientImpl.java
+++ src/main/java/no/nav/fo/veilarbdialog/oppfolging/v2/OppfolgingV2ClientImpl.java
@@ -4,103 +4,43 @@ import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.client.aktoroppslag.AktorOppslagClient;
-import no.nav.common.rest.client.RestUtils;
 import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
+import no.nav.fo.veilarbdialog.config.PortefoljeClient;
 import no.nav.fo.veilarbdialog.oppfolging.siste_periode.GjeldendePeriodeMetrikk;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Supplier;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class OppfolgingV2ClientImpl implements OppfolgingV2Client {
-    private final OkHttpClient client;
     private final AktorOppslagClient aktorOppslagClient;
     private final GjeldendePeriodeMetrikk gjeldendePeriodeMetrikk;
-    private final Supplier<String> machineToMachineTokenProvider;
-
-    @Value("${application.veilarboppfolging.api.url}")
-    private String baseUrl;
+    private final PortefoljeClient portefoljeClient;
 
     public Optional<OppfolgingV2UnderOppfolgingDTO> fetchUnderoppfolging(AktorId aktorId) {
-        Fnr fnr = aktorOppslagClient.hentFnr(aktorId);
-
-        String uri = String.format("%s/v2/oppfolging?fnr=%s", baseUrl, fnr.get());
-
-        Request request = new Request.Builder()
-                .url(uri)
-                .header(AUTHORIZATION, "Bearer " + machineToMachineTokenProvider.get())
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            RestUtils.throwIfNotSuccessful(response);
-            return RestUtils.parseJsonResponse(response, OppfolgingV2UnderOppfolgingDTO.class);
-        } catch (Exception e) {
-            throw internalServerError(e, request.url().toString());
-        }
+        var fnr = aktorOppslagClient.hentFnr(aktorId);
+        var uri = String.format("/v2/oppfolging?fnr=%s", fnr.get());
+        return portefoljeClient.request(uri, OppfolgingV2UnderOppfolgingDTO.class);
     }
 
     @Override
     @Timed
     public Optional<OppfolgingPeriodeMinimalDTO> fetchGjeldendePeriode(AktorId aktorId) {
         Fnr fnr = aktorOppslagClient.hentFnr(aktorId);
-
-        String uri = String.format("%s/v2/oppfolging/periode/gjeldende?fnr=%s", baseUrl, fnr.get());
-        Request request = new Request.Builder()
-                .url(uri)
-                .header(AUTHORIZATION, "Bearer " + machineToMachineTokenProvider.get())
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            RestUtils.throwIfNotSuccessful(response);
-            if (response.code() == HttpStatus.NO_CONTENT.value()) {
-                gjeldendePeriodeMetrikk.tellKallTilEksternOppfolgingsperiode(false);
-                return Optional.empty();
-            }
-            gjeldendePeriodeMetrikk.tellKallTilEksternOppfolgingsperiode(true);
-            return RestUtils.parseJsonResponse(response, OppfolgingPeriodeMinimalDTO.class);
-        } catch (Exception e) {
-            throw internalServerError(e, request.url().toString());
-        }
+        String uri = String.format("/v2/oppfolging/periode/gjeldende?fnr=%s", fnr.get());
+        var response = portefoljeClient.request(uri, OppfolgingPeriodeMinimalDTO.class);
+        gjeldendePeriodeMetrikk.tellKallTilEksternOppfolgingsperiode(response.isPresent());
+        return response;
     }
 
     @Timed
     @Override
     public Optional<List<OppfolgingPeriodeMinimalDTO>> hentOppfolgingsperioder(AktorId aktorId) {
         Fnr fnr = aktorOppslagClient.hentFnr(aktorId);
-
-        String uri = String.format("%s/v2/oppfolging/perioder?fnr=%s", baseUrl, fnr.get());
-        Request request = new Request.Builder()
-                .url(uri)
-                .header(AUTHORIZATION, "Bearer " + machineToMachineTokenProvider.get())
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            RestUtils.throwIfNotSuccessful(response);
-            if (response.code() == HttpStatus.NO_CONTENT.value()) {
-                return Optional.empty();
-            }
-            return RestUtils.parseJsonArrayResponse(response, OppfolgingPeriodeMinimalDTO.class);
-        } catch (Exception e) {
-            throw internalServerError(e, request.url().toString());
-        }
-    }
-
-    private ResponseStatusException internalServerError(Exception cause, String url) {
-        return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, String.format("Feil ved kall mot %s", url), cause);
-    }
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
+        String uri = String.format("/v2/oppfolging/perioder?fnr=%s", fnr.get());
+        return portefoljeClient.requestArrayData(uri, OppfolgingPeriodeMinimalDTO.class);
     }
 }
