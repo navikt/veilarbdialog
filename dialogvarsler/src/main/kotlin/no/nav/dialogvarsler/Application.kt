5bce933aac7139b7460a8fa9f71be1c6b2d0572e
sigurdgroneng <sigurd.groneng@nav.no>
2023-09-04 11:13:20 +0200

    EngineMain

--- dialogvarsler/src/main/kotlin/no/nav/dialogvarsler/Application.kt
+++ dialogvarsler/src/main/kotlin/no/nav/dialogvarsler/Application.kt
@@ -5,10 +5,12 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import no.nav.dialogvarsler.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+//fun main() {
+//    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+//        .start(wait = true)
+//}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
 
 fun Application.module() {
     configureAuthentication()
