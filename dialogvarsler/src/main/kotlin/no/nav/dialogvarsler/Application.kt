10f74dd13607d94ff882c2136d82ec9f8cd03955
sigurdgroneng <sigurd.groneng@nav.no>
2023-09-05 12:16:16 +0200

    Add catch-all thread logging

--- dialogvarsler/src/main/kotlin/no/nav/dialogvarsler/Application.kt
+++ dialogvarsler/src/main/kotlin/no/nav/dialogvarsler/Application.kt
@@ -4,12 +4,19 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import no.nav.dialogvarsler.plugins.*
+import org.slf4j.LoggerFactory
 
 //fun main() {
 //    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
 //        .start(wait = true)
 //}
-fun main(args: Array<String>): Unit = EngineMain.main(args)
+fun main(args: Array<String>): Unit {
+    EngineMain.main(args)
+    val logger = LoggerFactory.getLogger(Application::class.java)
+    Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
+        logger.error("Uncaught exception i thread: ${thread.name}", exception)
+    }
+}
 
 
 fun Application.module() {
