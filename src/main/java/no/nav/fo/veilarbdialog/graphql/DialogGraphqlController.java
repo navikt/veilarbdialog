2673969da8abad26819440393220c247a6ccb9c2
sigurdgroneng <sigurd.groneng@nav.no>
2024-04-12 11:07:19 +0200

    Legg til kontorsperre

--- src/main/java/no/nav/fo/veilarbdialog/graphql/DialogGraphqlController.java
+++ src/main/java/no/nav/fo/veilarbdialog/graphql/DialogGraphqlController.java
@@ -3,7 +3,9 @@ package no.nav.fo.veilarbdialog.graphql;
 import lombok.AllArgsConstructor;
 import no.nav.common.types.identer.Fnr;
 import no.nav.fo.veilarbdialog.domain.DialogDTO;
+import no.nav.fo.veilarbdialog.domain.DialogData;
 import no.nav.fo.veilarbdialog.domain.Person;
+import no.nav.fo.veilarbdialog.kvp.KontorsperreFilter;
 import no.nav.fo.veilarbdialog.rest.RestMapper;
 import no.nav.fo.veilarbdialog.service.DialogDataService;
 import no.nav.poao.dab.spring_auth.AuthService;
@@ -15,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 @AllArgsConstructor
 @Controller
@@ -23,6 +26,8 @@ public class DialogGraphqlController {
     final private AuthService authService;
     final private DialogDataService dialogDataService;
     final private RestMapper restMapper;
+    final private KontorsperreFilter kontorsperreFilter;
+
 
     @QueryMapping
     public List<DialogDTO> dialoger(@Argument String fnr, @Argument Optional<Boolean> bareMedAktiviteter) {
@@ -30,14 +35,19 @@ public class DialogGraphqlController {
         authService.sjekkTilgangTilPerson(targetFnr);
         return dialogDataService.hentDialogerForBruker(Person.fnr(targetFnr.get()))
                 .stream()
-                .filter(dialog -> {
-                    if (bareMedAktiviteter.orElse(false)) {
-                        return dialog.getAktivitetId() != null;
-                    } else {
-                        return true;
-                    }
-                })
+                .filter(bareMedAktiviteterFilter(bareMedAktiviteter))
+                .filter(kontorsperreFilter::tilgangTilEnhet)
                 .map(restMapper::somDialogDTO).toList();
+    }
+
+    private Predicate<DialogData> bareMedAktiviteterFilter(Optional<Boolean> bareMedAktiviteter) {
+        return dialog -> {
+            if (bareMedAktiviteter.orElse(false)) {
+                return dialog.getAktivitetId() != null;
+            } else {
+                return true;
+            }
+        };
     }
 
     private Fnr getContextUserIdent(String fnr) {
