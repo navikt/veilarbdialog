cf1599eacad02e35f72117f0e270d35cb4444e06
sigurdgroneng <sigurd.groneng@nav.no>
2024-08-09 10:51:04 +0200

    Tøm migrations mappa, flytt til patch

--- src/main/resources/db/migration/V3__midlertidig-bruker-rettigheter.sql
+++
@@ -1,3 +0,0 @@
-grant usage on schema veilarbdialog to "veilarbdialog_midlertidig";
-grant select, insert, delete, update on table shedlock to "veilarbdialog_midlertidig";
-
--- src/main/resources/db/patch/rettigheter-midlertidig-bruker.sql
+++
@@ -1,4 +0,0 @@
-grant usage on schema veilarbdialog to "veilarbdialog_midlertidig";
-grant select, insert, delete, update on table shedlock to "veilarbdialog_midlertidig";
-
-grant select on table schema_version to "veilarbdialog_midlertidig";
