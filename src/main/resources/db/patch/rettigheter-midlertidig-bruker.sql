2458226712fa1627e52fb22e783206882fb05de6
Mads Lee Giil <mads.lee.giil@nav.no>
2024-08-09 10:25:38 +0200

    Lag patch-fil for å dokumentere hva vi gjør manuelt mot databasen

---
+++ src/main/resources/db/patch/rettigheter-midlertidig-bruker.sql
@@ -0,0 +1,4 @@
+grant usage on schema veilarbdialog to "veilarbdialog_midlertidig";
+grant select, insert, delete, update on table shedlock to "veilarbdialog_midlertidig";
+
+grant select on table schema_version to "veilarbdialog_midlertidig";
