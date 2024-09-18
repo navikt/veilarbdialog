93662c0537101601e2b2cbd440036009c610e291
sigurdgroneng <sigurd.groneng@nav.no>
2024-09-18 09:28:46 +0200

    Ikkje bruk fnr i url

--- src/main/java/no/nav/fo/veilarbdialog/oppfolging/v2/OppfolgingV2ClientImpl.java
+++ src/main/java/no/nav/fo/veilarbdialog/oppfolging/v2/OppfolgingV2ClientImpl.java
@@ -4,12 +4,16 @@ import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.client.aktoroppslag.AktorOppslagClient;
+import no.nav.common.json.JsonUtils;
 import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
 import no.nav.fo.veilarbdialog.clients.util.HttpClientWrapper;
 import no.nav.fo.veilarbdialog.clients.veilarboppfolging.ManuellStatusV2DTO;
 import no.nav.fo.veilarbdialog.clients.veilarboppfolging.UnderOppfolgingDTO;
 import no.nav.fo.veilarbdialog.oppfolging.siste_periode.GjeldendePeriodeMetrikk;
+import no.nav.fo.veilarbdialog.rest.FnrDto;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -27,9 +31,11 @@ public class OppfolgingV2ClientImpl implements OppfolgingV2Client {
 
     @Override
     public boolean erUnderOppfolging(Fnr fnr) {
-        String uri = String.format("/v2/oppfolging?fnr=%s", fnr.get());
+        String uri = "/v3/hent-oppfolging";
+        var fnrDto = new FnrDto(fnr.get());
+        var requestBody = RequestBody.create(JsonUtils.toJson(fnrDto), MediaType.parse("application/json"));
         return veilarboppfolgingClientWrapper
-                .get(uri, UnderOppfolgingDTO.class)
+                .postAndReceive(uri, requestBody, UnderOppfolgingDTO.class)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Feil ved kall mot oppfolging"))
                 .isErUnderOppfolging();
     }
@@ -38,8 +44,10 @@ public class OppfolgingV2ClientImpl implements OppfolgingV2Client {
     @Timed
     public Optional<OppfolgingPeriodeMinimalDTO> fetchGjeldendePeriode(AktorId aktorId) {
         Fnr fnr = aktorOppslagClient.hentFnr(aktorId);
-        String uri = String.format("/v2/oppfolging/periode/gjeldende?fnr=%s", fnr.get());
-        var response = veilarboppfolgingClientWrapper.get(uri, OppfolgingPeriodeMinimalDTO.class);
+        String uri = "/v3/oppfolging/periode/hent-gjeldende-periode";
+        var fnrDto = new FnrDto(fnr.get());
+        var requestBody = RequestBody.create(JsonUtils.toJson(fnrDto), MediaType.parse("application/json"));
+        var response = veilarboppfolgingClientWrapper.postAndReceive(uri, requestBody, OppfolgingPeriodeMinimalDTO.class);
         gjeldendePeriodeMetrikk.tellKallTilEksternOppfolgingsperiode(response.isPresent());
         return response;
     }
@@ -48,13 +56,17 @@ public class OppfolgingV2ClientImpl implements OppfolgingV2Client {
     @Override
     public Optional<List<OppfolgingPeriodeMinimalDTO>> hentOppfolgingsperioder(AktorId aktorId) {
         Fnr fnr = aktorOppslagClient.hentFnr(aktorId);
-        String uri = String.format("/v2/oppfolging/perioder?fnr=%s", fnr.get());
-        return veilarboppfolgingClientWrapper.getList(uri, OppfolgingPeriodeMinimalDTO.class);
+        String uri = "/v3/oppfolging/hent-perioder";
+        var fnrDto = new FnrDto(fnr.get());
+        var requestBody = RequestBody.create(JsonUtils.toJson(fnrDto), MediaType.parse("application/json"));
+        return veilarboppfolgingClientWrapper.postAndReceiveList(uri, requestBody, OppfolgingPeriodeMinimalDTO.class);
     }
 
     @Override
     public Optional<ManuellStatusV2DTO> hentManuellStatus(Fnr fnr) {
-        String uri = String.format("/v2/manuell/status?fnr=%s", fnr.get());
-        return veilarboppfolgingClientWrapper.get(uri, ManuellStatusV2DTO.class);
+        String uri = "/v3/manuell/hent-status";
+        var fnrDto = new FnrDto(fnr.get());
+        var requestBody = RequestBody.create(JsonUtils.toJson(fnrDto), MediaType.parse("application/json"));
+        return veilarboppfolgingClientWrapper.postAndReceive(uri, requestBody, ManuellStatusV2DTO.class);
     }
 }
