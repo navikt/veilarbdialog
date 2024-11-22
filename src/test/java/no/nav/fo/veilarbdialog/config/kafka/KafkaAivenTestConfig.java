9385e2ec073ddfad4ede38f2a78becdead9d13f8
sigurdgroneng <sigurd.groneng@nav.no>
2024-11-22 15:02:47 +0100

    Fjern avro

--- src/test/java/no/nav/fo/veilarbdialog/config/kafka/KafkaAivenTestConfig.java
+++ src/test/java/no/nav/fo/veilarbdialog/config/kafka/KafkaAivenTestConfig.java
@@ -1,8 +1,6 @@
 package no.nav.fo.veilarbdialog.config.kafka;
 
-import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.admin.Admin;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.ssl.DefaultSslBundleRegistry;
@@ -10,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
@@ -43,20 +40,6 @@ public class KafkaAivenTestConfig {
         return new DefaultKafkaProducerFactory<>(producerProperties);
     }
     // ******* Skrive til siste_oppfolginsperiode_topic og aktivitetskort-idmappinger i test SLUTT ***********
-
-
-    // ******* Lese fra brukernotifikasjons topics i test START ***********
-    @Bean("avroAvroConsumerFactory")
-    @Primary
-    <K extends SpecificRecordBase, V extends SpecificRecordBase> ConsumerFactory<K, V> avroAvroConsumerFactory(KafkaProperties kafkaProperties, EmbeddedKafkaBroker embeddedKafka) {
-        Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties(new DefaultSslBundleRegistry());
-        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafka.getBrokersAsString());
-        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroDeserializer.class);
-        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroDeserializer.class);
-        consumerProperties.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        return new DefaultKafkaConsumerFactory<>(consumerProperties);
-    }
-    // ******* Lese fra brukernotifikasjons topics i test SLUTT ***********
 
     @Bean
     public Admin kafkaAdminClient(KafkaProperties properties, EmbeddedKafkaBroker embeddedKafkaBroker) {
