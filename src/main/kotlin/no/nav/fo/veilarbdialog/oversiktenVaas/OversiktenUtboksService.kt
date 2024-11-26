3fe31fb5dc787be7cb7dd3360d42ad9d8660f8d3
Mads Lee Giil <mads.lee.giil@nav.no>
2024-11-26 13:30:01 +0100

    Send melding på Kafka

--- src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
+++ src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
@@ -1,10 +1,12 @@
 package no.nav.fo.veilarbdialog.oversiktenVaas
 
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import no.nav.common.client.aktoroppslag.AktorOppslagClient
 import no.nav.common.json.JsonUtils
 import no.nav.common.types.identer.AktorId
 import no.nav.common.types.identer.Fnr
 import no.nav.fo.veilarbdialog.eskaleringsvarsel.entity.EskaleringsvarselEntity
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.util.*
@@ -12,20 +14,30 @@ import java.util.*
 @Service
 open class OversiktenUtboksService(
     private val aktorOppslagClient: AktorOppslagClient,
-    private val  oversiktenUtboksRepository: OversiktenUtboksRepository
+    private val oversiktenUtboksRepository: OversiktenUtboksRepository,
+    private val oversiktenProducer: OversiktenProducer
 ) {
+
+    @Scheduled(cron = "0 */5 * * * *") // Hvert 5. minutt
+    @SchedulerLock(name = "oversikten_utboks_scheduledTask", lockAtMostFor = "PT3M")
+    open fun sendUtgåtteVarslerTilOversikten() {
+        val meldingerSomSkalSendes = oversiktenUtboksRepository.hentAlleSomSkalSendes()
+        meldingerSomSkalSendes.forEach { melding ->
+            oversiktenProducer.sendMelding(melding.meldingKey.toString(), melding.meldingSomJson)
+            // TODO: Marker sendt
+        }
+    }
 
     fun sendMeldingTilOversikten(gjeldendeEskaleringsvarsler: List<EskaleringsvarselEntity> ) {
         gjeldendeEskaleringsvarsler.forEach {
             val fnr =  aktorOppslagClient.hentFnr(AktorId(it.aktorId))
-            val melding = it.tilMelding(Operasjon.START, fnr)
+            val melding = utgåttVarselMelding(Operasjon.START, fnr)
             val sendingEntity = SendingEntity(meldingSomJson = JsonUtils.toJson(melding), fnr = fnr, kategori = melding.kategori, meldingKey = UUID.randomUUID())
             oversiktenUtboksRepository.lagreSending(sendingEntity)
         }
-
     }
 
-    private fun EskaleringsvarselEntity.tilMelding(operasjon: Operasjon, fnr: Fnr) =
+    private fun utgåttVarselMelding(operasjon: Operasjon, fnr: Fnr) =
         OversiktenUtboksMelding(
             personID = fnr.get(),
             kategori = Kategori.UTGATT_VARSEL,
