936465799c130cc83fa09f5266a2729dcb193a2c
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-11-26 15:14:48 +0100

    lagt til egen fil for OversiktenMelding

---
+++ src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenMelding.kt
@@ -0,0 +1,42 @@
+package no.nav.fo.veilarbdialog.oversiktenVaas
+
+import java.time.LocalDateTime
+
+
+data class OversiktenUtboksMelding(
+    val personID: String,
+    val avsender: String = "veilarbdialog",
+    val kategori: Kategori,
+    val operasjon: Operasjon,
+    val hendelse: Hendelse
+) {
+    companion object {
+        fun forUtgattVarsel(fnr: String) = OversiktenUtboksMelding(
+            personID = fnr,
+            kategori = Kategori.UTGATT_VARSEL,
+            operasjon = Operasjon.START,
+            hendelse = Hendelse(
+                beskrivelse = "Bruker har et utgått varsel",
+                dato = LocalDateTime.now(),
+                lenke = "url", // TODO: fiks ordentlig url
+            )
+        )
+    }
+}
+
+data class Hendelse (
+    val beskrivelse: String,
+    val dato: LocalDateTime,
+    val lenke: String,
+    val detaljer: String? = null,
+)
+
+enum class Kategori {
+    UTGATT_VARSEL
+}
+
+enum class Operasjon {
+    START,
+    OPPDATER,
+    STOPP
+}
--- src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
+++ src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
@@ -4,11 +4,9 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import no.nav.common.client.aktoroppslag.AktorOppslagClient
 import no.nav.common.json.JsonUtils
 import no.nav.common.types.identer.AktorId
-import no.nav.common.types.identer.Fnr
 import no.nav.fo.veilarbdialog.eskaleringsvarsel.entity.EskaleringsvarselEntity
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 import java.util.*
 
 @Service
@@ -28,49 +26,17 @@ open class OversiktenUtboksService(
         }
     }
 
-    open fun sendMeldingTilOversikten(gjeldendeEskaleringsvarsler: List<EskaleringsvarselEntity> ) {
+    open fun sendMeldingTilOversikten(gjeldendeEskaleringsvarsler: List<EskaleringsvarselEntity>) {
         gjeldendeEskaleringsvarsler.forEach {
-            val fnr =  aktorOppslagClient.hentFnr(AktorId(it.aktorId))
-            val melding = utgåttVarselMelding(Operasjon.START, fnr)
-            val sendingEntity = SendingEntity(meldingSomJson = JsonUtils.toJson(melding), fnr = fnr, kategori = melding.kategori, meldingKey = UUID.randomUUID())
+            val fnr = aktorOppslagClient.hentFnr(AktorId(it.aktorId))
+            val melding = OversiktenUtboksMelding.forUtgattVarsel(fnr.toString())
+            val sendingEntity = SendingEntity(
+                meldingSomJson = JsonUtils.toJson(melding),
+                fnr = fnr,
+                kategori = melding.kategori,
+                meldingKey = UUID.randomUUID()
+            )
             oversiktenUtboksRepository.lagreSending(sendingEntity)
         }
-    }
-
-    private fun utgåttVarselMelding(operasjon: Operasjon, fnr: Fnr) =
-        OversiktenUtboksMelding(
-            personID = fnr.get(),
-            kategori = Kategori.UTGATT_VARSEL,
-            operasjon = operasjon,
-            hendelse = Hendelse(
-                beskrivelse = "Bruker har et utgått varsel",
-                dato = LocalDateTime.now(),
-                lenke = "url", // TODO: fiks ordentlig url
-            )
-        )
-
-    data class OversiktenUtboksMelding(
-        val personID: String,
-        val avsender: String = "veilarbdialog",
-        val kategori: Kategori,
-        val operasjon: Operasjon,
-        val hendelse: Hendelse
-    )
-
-    data class Hendelse (
-        val beskrivelse: String,
-        val dato: LocalDateTime,
-        val lenke: String,
-        val detaljer: String? = null,
-    )
-
-    enum class Kategori {
-        UTGATT_VARSEL
-    }
-
-    enum class Operasjon {
-        START,
-        OPPDATER,
-        STOPP
     }
 }
