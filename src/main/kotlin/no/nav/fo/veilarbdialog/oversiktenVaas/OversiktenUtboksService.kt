7b4592eaf38be8a42e0eb343b803bb2d5cb966a0
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-11-26 08:41:39 +0100

    sett opp data class

--- src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
+++ src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
@@ -2,6 +2,7 @@ package no.nav.fo.veilarbdialog.oversiktenVaas
 
 import no.nav.fo.veilarbdialog.eskaleringsvarsel.entity.EskaleringsvarselEntity
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 open class OversiktenUtboksService(
@@ -9,5 +10,31 @@ open class OversiktenUtboksService(
 
     fun sendMeldingTilOversikten(eskaleringsvarsler: List<EskaleringsvarselEntity> ) {
 
+
+
+    }
+    data class OversiktenUtboksMelding(
+        val personID: String,
+        val avsender: String,
+        val kategori: Kategori,
+        val operasjon: Operasjon,
+        val hendelse: Hendelse
+    )
+
+    data class Hendelse (
+        val navn: String,
+        val dato: LocalDateTime,
+        val lenke: String,
+        val detaljer: String?,
+    )
+
+    enum class Kategori {
+        UTGATT_VARSEL
+    }
+
+    enum class Operasjon {
+        START,
+        OPPDATER,
+        STOPP
     }
 }
