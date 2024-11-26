f8af3bc6704925161d1a408820fa9a2deaeabcaf
johannetronstad <johanne.tronstad@nav.no>
2024-11-26 09:01:23 +0100

    Map eskaleringsvarselentity til melding

--- src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
+++ src/main/kotlin/no/nav/fo/veilarbdialog/oversiktenVaas/OversiktenUtboksService.kt
@@ -8,24 +8,38 @@ import java.time.LocalDateTime
 open class OversiktenUtboksService(
 ) {
 
-    fun sendMeldingTilOversikten(eskaleringsvarsler: List<EskaleringsvarselEntity> ) {
-
-
+    fun sendMeldingTilOversikten(gjeldendeEskaleringsvarsler: List<EskaleringsvarselEntity> ) {
+        gjeldendeEskaleringsvarsler.forEach {
+            val melding = it.tilMelding(Operasjon.START)
+        }
 
     }
+
+    private fun EskaleringsvarselEntity.tilMelding(operasjon: Operasjon) =
+        OversiktenUtboksMelding(
+            personID = this.aktorId, // TODO: map til fnr
+            kategori = Kategori.UTGATT_VARSEL,
+            operasjon = operasjon,
+            hendelse = Hendelse(
+                beskrivelse = "Bruker har et utgått varsel",
+                dato = LocalDateTime.now(),
+                lenke = "url", // TODO: fiks ordentlig url
+            )
+        )
+
     data class OversiktenUtboksMelding(
         val personID: String,
-        val avsender: String,
+        val avsender: String = "veilarbdialog",
         val kategori: Kategori,
         val operasjon: Operasjon,
         val hendelse: Hendelse
     )
 
     data class Hendelse (
-        val navn: String,
+        val beskrivelse: String,
         val dato: LocalDateTime,
         val lenke: String,
-        val detaljer: String?,
+        val detaljer: String? = null,
     )
 
     enum class Kategori {
