d49eeedccf391ed2403098dbbfc0d469a1cbaf2e
Mads Lee Giil <mads.lee.giil@nav.no>
2024-11-27 23:20:44 +0100

    Fjern ubrukt import

--- src/main/java/no/nav/fo/veilarbdialog/eskaleringsvarsel/EskaleringsvarselRepository.java
+++ src/main/java/no/nav/fo/veilarbdialog/eskaleringsvarsel/EskaleringsvarselRepository.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
