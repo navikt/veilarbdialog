83626c208b4afd160c72631d2f1b750cffdd4d5d
johannetronstad <johanne.tronstad@nav.no>
2024-12-04 11:59:40 +0100

    Gjør klasse open

--- src/main/kotlin/no/nav/fo/veilarbdialog/eventsLogger/BigQueryMetrikkCron.kt
+++ src/main/kotlin/no/nav/fo/veilarbdialog/eventsLogger/BigQueryMetrikkCron.kt
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Service
 
 @Service
 @EnableScheduling
-class BigQueryMetrikkCron(
+open class BigQueryMetrikkCron(
     val bigQueryClient: BigQueryClient,
     private val antallUtgattDAO: AntallUtgattDAO,
 ) {
 
     @Scheduled(cron = "0 */10 * * * *") // TODO: sendre til cron = "@midnight" før prod
     @SchedulerLock(name = "utgåtteVarsler_bigquery_metrikker", lockAtMostFor = "PT2M")
-    fun hentUtgåtteVarslerCron() {
+    open fun hentUtgåtteVarslerCron() {
         val antallUtgåtteVarsler = antallUtgattDAO.hentAntallUtgåtteVarsler()
         bigQueryClient.logAntallUtgåtteVarsler(antallUtgåtteVarsler)
     }
