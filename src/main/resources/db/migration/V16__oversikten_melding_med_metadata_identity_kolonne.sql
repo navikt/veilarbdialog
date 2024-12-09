4c9b0ff6247524af940fb16dea8bb56ec0bbb58f
Mads Lee Giil <mads.lee.giil@nav.no>
2024-12-09 11:57:58 +0100

    WIP: Tenk på databasetabelldesign

--- src/main/resources/db/migration/V16__oversikten_melding_med_metadata_identity_kolonne.sql
+++ src/main/resources/db/migration/V16__oversikten_melding_med_metadata_identity_kolonne.sql
@@ -1,15 +1,21 @@
 drop table oversikten_melding_med_metadata;
-CREATE TYPE OVERSIKTEN_UTSENDING_STATUS AS ENUM ('SKAL_STARTES', 'SKAL_STOPPES', 'STARTET', 'STOPPET', 'ABORTERT');
-create table oversikten_melding_med_metadata (
-                                                 melding_key                  uuid not null,
-                                                 fnr                          varchar(11)     not null,
-                                                 opprettet                    timestamp       not null,
-                                                 tidspunkt_startet              timestamp,
-                                                 tidspunkt_stoppet              timestamp,
-                                                 utsending_status             OVERSIKTEN_UTSENDING_STATUS  not null,
-                                                 melding                      json            not null,
-                                                 kategori                     text            not null
+
+-- Eller
+CREATE TYPE OVERSIKTEN_UTSENDING_STATUS AS ENUM ('SKAL_SENDES', 'SENDT', 'SKAL_IKKE_SENDES');
+CREATE TYPE OVERSIKTEN_OPERASJON AS ENUM ('START', 'OPPDATER', 'STOPP');
+CREATE TYPE OVERSIKTEN_KATEGORI AS ENUM ('UTGATT_VARSEL');
+
+create table oversikten_melding_med_metadata
+(
+    id               SERIAL PRIMARY KEY,
+    melding_key      uuid                        not null,
+    fnr              varchar(11)                 not null,
+    opprettet        timestamp                   not null,
+    tidspunkt_sendt  timestamp,
+    utsending_status OVERSIKTEN_UTSENDING_STATUS not null,
+    melding          json                        not null,
+    kategori         OVERSIKTEN_KATEGORI         not null,
+    operasjon        OVERSIKTEN_OPERASJON        not null
 );
-create unique index oversikten_melding_med_metadata_melding_key_pk on oversikten_melding_med_metadata(melding_key);
-alter table oversikten_melding_med_metadata add primary key using index oversikten_melding_med_metadata_melding_key_pk;
-create index oversikten_melding_med_metadata_utsending_status_idx on oversikten_melding_med_metadata(utsending_status);
+create index oversikten_melding_med_metadata_melding_key_pk on oversikten_melding_med_metadata (melding_key);
+create index oversikten_melding_med_metadata_utsending_status_idx on oversikten_melding_med_metadata (utsending_status);
