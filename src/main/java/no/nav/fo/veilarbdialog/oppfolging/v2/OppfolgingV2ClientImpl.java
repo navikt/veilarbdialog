607f446cb816e56928e1730f5de39c26d6ec3e87
erikbosnes <erik.bosnes@nav.no>
2025-04-11 13:14:14 +0200

    Fiks uri for å hente gjeldende periode

--- src/main/java/no/nav/fo/veilarbdialog/oppfolging/v2/OppfolgingV2ClientImpl.java
+++ src/main/java/no/nav/fo/veilarbdialog/oppfolging/v2/OppfolgingV2ClientImpl.java
@@ -43,8 +43,8 @@ public class OppfolgingV2ClientImpl implements OppfolgingV2Client {
     @Override
     @Timed
     public Optional<OppfolgingPeriodeMinimalDTO> fetchGjeldendePeriode(AktorId aktorId) {
-        Fnr fnr = aktorOppslagClient.hentFnr(aktorId);
-        String uri = "/v3/oppfolging/periode/hent-gjeldende";
+        var fnr = aktorOppslagClient.hentFnr(aktorId);
+        var uri = "/v3/oppfolging/periode/hent-gjeldende-periode";
         var fnrDto = new FnrDto(fnr.get());
         var requestBody = RequestBody.create(JsonUtils.toJson(fnrDto), MediaType.parse("application/json"));
         var response = veilarboppfolgingClientWrapper.postAndReceive(uri, requestBody, OppfolgingPeriodeMinimalDTO.class);
