a75013be23a4d18b801a75825cfdbc5b0d892f5d
sigurdgroneng <sigurd.groneng@nav.no>
2025-12-18 16:25:34 +0100

    Hent kontorsperre fra graphql i veilarboppfolging

--- src/test/java/no/nav/fo/veilarbdialog/kvp/KvpConfig.java
+++ src/test/java/no/nav/fo/veilarbdialog/kvp/KvpConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -16,7 +17,7 @@ public class KvpConfig {
 
     @PostConstruct
     public void postConstruct() {
-        when(kvpService.kontorsperreEnhetId(anyString())).thenReturn("KONTORSPERRE_ENHET_ID");
+        when(kvpService.kontorsperreEnhetId(any())).thenReturn("KONTORSPERRE_ENHET_ID");
     }
 
     @Primary
