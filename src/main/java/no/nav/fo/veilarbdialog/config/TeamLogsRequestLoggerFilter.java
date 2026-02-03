c633ad2a14d7499095120c1ad7302a5d0a21841c
Mads Lee Giil <74543378+madsleegiil@users.noreply.github.com>
2026-02-03 12:20:44 +0100

    Bytt ut secureLogs med TeamLogs (#425)
    * Bytt ut secureLogs med teamLogs
    
    * Legg til linje for å verifisere teamLogs
    
    * Flytt testlinje til mer brukt endepunkt
    
    * Fjern testlinje
    
    * Logg ident kun i filter som går til teamlogs
    
    * Fjern ubrukt klasse

--- src/main/java/no/nav/fo/veilarbdialog/config/TeamLogsRequestLoggerFilter.java
+++ src/main/java/no/nav/fo/veilarbdialog/config/TeamLogsRequestLoggerFilter.java
@@ -3,44 +3,39 @@ package no.nav.fo.veilarbdialog.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+import lombok.RequiredArgsConstructor;
+import no.nav.poao.dab.spring_auth.IAuthService;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 import static java.lang.String.format;
-import static no.nav.fo.veilarbdialog.config.EnhanceSecureLogsFilter.SECURELOGS_ER_INTERN_BRUKER;
-import static no.nav.fo.veilarbdialog.config.EnhanceSecureLogsFilter.SECURELOGS_INNLOGGET_BRUKER_IDENT;
+import static no.nav.util.TeamLog.teamLog;
 
-public class SecureRequestLoggerFilter implements Filter {
+@Service
+@RequiredArgsConstructor
+public class TeamLogsRequestLoggerFilter implements Filter {
 
-    private final Logger secureLog = LoggerFactory.getLogger("SecureLog");
+    private final IAuthService authService;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         filterChain.doFilter(servletRequest, servletResponse);
-
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        String erInternBruker = Boolean.toString(authService.erInternBruker());
+        String innloggetBrukerIdent = authService.getLoggedInnUser().get();
 
         String msg = format("status=%s method=%s host=%s path=%s erInternBruker=%s innloggetIdent=%s queryString=%s",
                 httpResponse.getStatus(),
                 httpRequest.getMethod(),
                 httpRequest.getServerName(),
                 httpRequest.getRequestURI(),
-                MDC.get(SECURELOGS_ER_INTERN_BRUKER),
-                MDC.get(SECURELOGS_INNLOGGET_BRUKER_IDENT),
+                erInternBruker,
+                innloggetBrukerIdent,
                 httpRequest.getQueryString()
         );
-        secureLog.info(msg);
-    }
-
-    @Override
-    public void destroy() {
-        MDC.remove(SECURELOGS_ER_INTERN_BRUKER);
-        MDC.remove(SECURELOGS_INNLOGGET_BRUKER_IDENT);
-
-        Filter.super.destroy();
+        teamLog.info(msg);
     }
 }
