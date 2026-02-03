000397932497c736b46295bd8b7b2d10012ff337
Mads Lee Giil <74543378+madsleegiil@users.noreply.github.com>
2026-02-03 14:10:37 +0100

    Bytt ut secureLogs med TeamLogs (#426)
    * Bytt ut secureLogs med teamLogs
    
    * Legg til linje for å verifisere teamLogs
    
    * Flytt testlinje til mer brukt endepunkt
    
    * Fjern testlinje

--- src/main/java/no/nav/fo/veilarbdialog/config/EnhanceTeamLogsFilter.java
+++ src/main/java/no/nav/fo/veilarbdialog/config/EnhanceTeamLogsFilter.java
@@ -10,20 +10,20 @@ import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
-public class EnhanceSecureLogsFilter implements Filter {
+public class EnhanceTeamLogsFilter implements Filter {
 
     private final IAuthService authService;
 
-    public static final String SECURELOGS_ER_INTERN_BRUKER = "SecureLogsFilter.erInternBruker";
-    public static final String SECURELOGS_INNLOGGET_BRUKER_IDENT = "SecureLogsFilter.innloggetBrukerIdent";
+    public static final String TEAMLOGS_ER_INTERN_BRUKER = "TeamLogsFilter.erInternBruker";
+    public static final String TEAMLOGS_INNLOGGET_BRUKER_IDENT = "TeamLogsFilter.innloggetBrukerIdent";
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         String erInternBruker = Boolean.toString(authService.erInternBruker());
         String innloggetBrukerIdent = authService.getLoggedInnUser().get();
 
-        MDC.put(SECURELOGS_ER_INTERN_BRUKER, erInternBruker);
-        MDC.put(SECURELOGS_INNLOGGET_BRUKER_IDENT, innloggetBrukerIdent);
+        MDC.put(TEAMLOGS_ER_INTERN_BRUKER, erInternBruker);
+        MDC.put(TEAMLOGS_INNLOGGET_BRUKER_IDENT, innloggetBrukerIdent);
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
